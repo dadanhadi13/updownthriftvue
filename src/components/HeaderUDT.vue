b2a2aaf5976f80fe167d66f82cff6d54ec4647e6
dadanhadi13 <dadanhadi13@gmail.com>
2021-08-06 03:16:16 +0700

    add projek
    push to github

---
+++ src/components/HeaderUDT.vue
@@ -0,0 +1,87 @@
+<template>
+            <!-- Header Section Begin -->
+    <header class="header-section">
+        <div class="header-top">
+            <div class="container">
+                <div class="ht-left">
+                    <div class="mail-service">
+                        <i class=" fa fa-envelope"></i> upandownthrift@gmail.com
+                    </div>
+                    <div class="phone-service">
+                        <i class=" fa fa-phone"></i> +62 85156362589
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="inner-header">
+                <div class="row">
+                    <div class="col-lg-3 col-md-3">
+                        <div class="logo">
+                            <a href="./index.html">
+                                <img src="img/logowebudt.png" alt="" />
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6"></div>
+                    <div class="col-lg-3 text-right col-md-3">
+                        <ul class="nav-right">
+                            <li class="cart-icon">
+                                Keranjang Belanja &nbsp;
+                                <a href="#">
+                                    <i class="icon_bag_alt"></i>
+                                    <span>3</span>
+                                </a>
+                                <div class="cart-hover">
+                                    <div class="select-items">
+                                        <table>
+                                            <tbody>
+                                                <tr>
+                                                    <td class="si-pic">
+                                                        <img src="img/select-product-1.jpg" alt="" />
+                                                    </td>
+                                                    <td class="si-text">
+                                                        <div class="product-selected">
+                                                            <p>$60.00 x 1</p>
+                                                            <h6>Kabino Bedside Table</h6>
+                                                        </div>
+                                                    </td>
+                                                    <td class="si-close">
+                                                        <i class="ti-close"></i>
+                                                    </td>
+                                                </tr>
+                                                <tr>
+                                                    <td class="si-pic">
+                                                        <img src="img/select-product-2.jpg" alt="" />
+                                                    </td>
+                                                    <td class="si-text">
+                                                        <div class="product-selected">
+                                                            <p>$60.00 x 1</p>
+                                                            <h6>Kabino Bedside Table</h6>
+                                                        </div>
+                                                    </td>
+                                                    <td class="si-close">
+                                                        <i class="ti-close"></i>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <div class="select-total">
+                                        <span>total:</span>
+                                        <h5>$120.00</h5>
+                                    </div>
+                                    <div class="select-button">
+                                        <a href="#" class="primary-btn view-card">VIEW CARD</a>
+                                        <a href="#" class="primary-btn checkout-btn">CHECK OUT</a>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header End -->
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+      <HeaderUDT/>
+      <HeroUDT/>
+      <BannerUDT/>
+      <InstaUDT/>
+      <PartnerLogoUDT/>
+      <FooterUDT/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderUDT from '@/components/HeaderUDT.vue'
+import HeroUDT from '@/components/HeroUDT.vue'
+import BannerUDT from '@/components/BannerUDT.vue'
+import InstaUDT from '@/components/InstaUDT.vue'
+import PartnerLogoUDT from '@/components/PartnerLogoUDT.vue'
+import FooterUDT from '@/components/FooterUDT.vue'
+
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderUDT,
+    HeroUDT,
+    BannerUDT,
+    InstaUDT,
+    PartnerLogoUDT,
+    FooterUDT
+  }
+}
+</script>
